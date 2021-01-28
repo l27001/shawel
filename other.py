e38e86e1f055e08bd935c4e5bc6c93d953623cd4
Mr_B <krack21@yandex.ru>
2021-01-28 18:33:02 +0400

    modified:   other.py

--- other.py
+++ other.py
@@ -1,8 +1,6 @@
 import vk, os
 import config
 
-def make_con():
-	return 
 session = vk.Session(access_token=config.access_token)
 api = vk.API(session, v='5.124', lang='ru')
 dir_path = os.path.dirname(os.path.realpath(__file__))
