c7470d733330a16bc8438eab177de335c5ab51df
Mr_B <krack21@yandex.ru>
2021-03-07 11:01:59 +0400

    modified:   commands.py

--- commands.py
+++ commands.py
@@ -87,11 +87,11 @@ class Commands:
             text[0] = text[0].lower()
             text[0] = text[0].replace('/','')
             userinfo.update({'replid':replid,'chat_id':chat_id, 'from_id':from_id, 'attachments':obj['attachments']})
-            cmds[text[0]](userinfo, text[1:])
-            if(text[0] not in cmds):
+            if(cmds.get(text[0]) == None):
                 if(chat_id < 2000000000):
                     Methods.send(chat_id, "👎🏻 Не понял.")
                 return None
+            cmds[text[0]](userinfo, text[1:])
         except Exception as e:
             Methods.log("ERROR", f"Непредвиденная ошибка. {str(e)}")
             Methods.send(chat_id, "⚠ Произошла непредвиденная ошибка.\nОбратитесь к @l27001", attachment="photo-183256712_457239188")
