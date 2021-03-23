1f0e4467defcdbfe6e33e88459cef61c13aabd9f
Mr_B <krack21@yandex.ru>
2021-03-23 19:55:45 +0400

    Улучшение uptime-parser'а

--- uptime_parse.py
+++ uptime_parse.py
@@ -4,34 +4,37 @@ from time import sleep
 from methods import Methods
 from config import uptimerobot_api
 
+def check_status(status, name):
+    if(status == 9 or status == 8):
+        return f"⚠ {name} Недоступен!"
+    elif(status == 0):
+        return f"❓ {name} Приостановлен"
+    elif(status == 2):
+        return f"✅ {name} Доступен"
+    else:
+        return f"❓ {kname} -> {status}"
+
+
 def job():
     res = requests.post('https://api.uptimerobot.com/v2/getMonitors?format=json', json={"api_key":uptimerobot_api}).json()
     monitors = res['monitors']
     stat = []
-    allu = Methods.mysql_query("SELECT * FROM uptime", "all")
+    allu = Methods.mysql_query("SELECT id, friendly_name FROM uptime", "all")
+    all_mon = {}
+    for k in allu:
+        all_mon.update({k['id']:k['friendly_name']})
     for k in monitors:
         data = Methods.mysql_query(f"SELECT * FROM uptime WHERE id='{k['id']}'")
         if(data == None):
             Methods.mysql_query(f"INSERT INTO uptime (`id`,`status`) VALUES ('{k['id']}','{k['status']}')")
-            if(k['status'] == 9):
-                stat.append(f"⚠ {k['friendly_name']} Недоступен!")
-        if(data['status'] != k['status']):
-            Methods.mysql_query(f"UPDATE uptime SET status='{k['status']}' WHERE id='{k['id']}'")
-            if(k['status'] == 9 or k['status'] == 8):
-                stat.append(f"⚠ {k['friendly_name']} Недоступен!")
-            elif(k['status'] == 0):
-                stat.append(f"❓ {k['friendly_name']} Приостановлен")
-            elif(k['status'] == 2):
-                stat.append(f"✅ {k['friendly_name']} Доступен")
-        i=0
-        for m in allu:
-            if(m['id'] == k['id']):
-                del(allu[i])
-                break
-            i+=1
-    for m in allu:
-        Methods.mysql_query(f"DELETE FROM uptime WHERE id='{m['id']}'")
-        stat.append(f"{m['id']} удалён!")
+            stat.append(check_status(k['status'], k['friendly_name']))
+        elif(data['status'] != k['status'] or data['friendly_name'] != k['friendly_name']):
+            Methods.mysql_query(f"UPDATE uptime SET status='{k['status']}', friendly_name='{k['friendly_name']}' WHERE id='{k['id']}'")
+            stat.append(check_status(k['status'], k['friendly_name']))
+        all_mon.pop(k['id'])
+    for id_, name in all_mon.items():
+        stat.append(f"{name} Удалён")
+        Methods.mysql_query(f"DELETE FROM uptime WHERE id='{id_}'")
     text = "\n".join(stat)
     if(len(text) > 0):
         Methods.send(331465308, text)
@@ -45,3 +48,4 @@ while True:
     except Exception as e:
         Methods.log("UptimeParser-ERROR", f"Произошла ошибка.\n\n{e}")
         Methods.send(331465308, f"С uptime-парсером что-то не так!\n\n{e}")
+        exit()
