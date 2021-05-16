de6eb93792931d24737afc201bebb05de9896b67
Mr_B <krack21@yandex.ru>
2021-05-16 22:17:17 +0400

    modified:   shawel.sql

--- shawel.sql
+++ shawel.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.5.9-MariaDB, for Linux (aarch64)
+-- MariaDB dump 10.19  Distrib 10.5.10-MariaDB, for Linux (aarch64)
 --
 -- Host: localhost    Database: shawel
 -- ------------------------------------------------------
--- Server version	10.5.9-MariaDB
+-- Server version	10.5.10-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,16 +29,6 @@ CREATE TABLE `api` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `api`
---
-
-LOCK TABLES `api` WRITE;
-/*!40000 ALTER TABLE `api` DISABLE KEYS */;
-INSERT INTO `api` VALUES ('d87174bf4cc2936c48a04a1ecac481975314c6877fe16b07768146523647104b');
-/*!40000 ALTER TABLE `api` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `chats`
 --
 
@@ -52,16 +42,6 @@ CREATE TABLE `chats` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `chats`
---
-
-LOCK TABLES `chats` WRITE;
-/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
-INSERT INTO `chats` VALUES (2000000002,0,0),(2000000015,0,1),(2000000016,1,0),(2000000017,0,0),(2000000018,0,0),(2000000019,0,0);
-/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `imgs`
@@ -78,17 +58,8 @@ CREATE TABLE `imgs` (
   `mark` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `mark` (`mark`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `imgs`
---
-
-LOCK TABLES `imgs` WRITE;
-/*!40000 ALTER TABLE `imgs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `imgs` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `mute`
@@ -106,15 +77,6 @@ CREATE TABLE `mute` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `mute`
---
-
-LOCK TABLES `mute` WRITE;
-/*!40000 ALTER TABLE `mute` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mute` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `uptime`
 --
 
@@ -128,16 +90,6 @@ CREATE TABLE `uptime` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `uptime`
---
-
-LOCK TABLES `uptime` WRITE;
-/*!40000 ALTER TABLE `uptime` DISABLE KEYS */;
-INSERT INTO `uptime` VALUES (786116008,'1. node1 [HTTPS]',2),(786815135,'2. node1 [PING]',2),(787219709,'6. node3 [PING]',2),(787368511,'5. node3 [HTTPS]',2),(787783921,'4. node2 [PING]',2),(787783931,'3. node2 [HTTPS]',2);
-/*!40000 ALTER TABLE `uptime` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -156,16 +108,6 @@ CREATE TABLE `users` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (228167601,0,2,1),(250700830,0,0,0),(252489312,0,0,0),(257842723,0,0,0),(277746917,0,0,0),(277988184,0,0,0),(279508220,0,0,0),(290137168,0,0,0),(301023575,0,0,0),(304126093,0,0,0),(305371382,0,0,0),(314544195,0,0,0),(319307323,0,0,0),(320809408,0,0,0),(324977194,0,0,0),(331465308,2,5,1),(332756622,0,0,0),(337744844,0,0,0),(339444055,0,0,0),(344493265,0,0,0),(356535517,0,1,1),(364911251,0,0,0),(381992762,0,107,0),(399941524,0,160,1),(406387825,0,0,0),(417079484,0,0,0),(445248149,0,0,0),(453268079,0,29,1),(487070830,0,0,0),(500136993,2,0,0),(524573062,0,5,0),(524876015,0,0,0),(539432007,0,0,0),(560289436,0,0,0),(564191377,0,0,0),(572689805,0,0,0),(574214420,0,4,1),(586788717,0,0,0),(587379423,0,0,0);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `vk`
 --
 
@@ -182,16 +124,6 @@ CREATE TABLE `vk` (
   `autopoliv` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vk`
---
-
-LOCK TABLES `vk` WRITE;
-/*!40000 ALTER TABLE `vk` DISABLE KEYS */;
-INSERT INTO `vk` VALUES ('photo331465308_457248908_5fcc1b0ff24bccd021,photo331465308_457248909_4aa35b02a2280f69c5,photo331465308_457248910_df104603f8d6dd814a,photo331465308_457248911_4352d06c703f2cd99a','photo331465308_457248912_cc21e9e25fc6bed8e7,photo331465308_457248913_60bd17ef6bae932f28','19:37:15 13.05.2021','20:26:49 13.05.2021',0,1617731100,1603791912);
-/*!40000 ALTER TABLE `vk` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -202,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-13 20:27:53
+-- Dump completed on 2021-05-16 22:17:13
